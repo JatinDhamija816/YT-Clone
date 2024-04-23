09f6a68192655d6a199239f024bb0feb9223fb32
JatinDhamija816 <jdhamija816@gmail.com>
2024-04-23 21:23:32 +0530

    util Api handle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv'
 import ConnectDB from "./db/index.js";
-
+import app from './app.js'
 dotenv.config()
 
 ConnectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(`MongoDb Connection Failed !!!! ${err}`)
+    })
